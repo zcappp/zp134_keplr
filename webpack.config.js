3d63f2824fa5047776c2a9cc91076f5d5b6d527f
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-02-13 08:47:54 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: process.env.mode || "development",
     devtool: false,
     entry: {
-        "zp134": "./plugins/zp134_keplr.js",
+        "zp134": "./src/zp134_keplr.js",
     },
     output: {
         path: path.join(__dirname, "build/"),
